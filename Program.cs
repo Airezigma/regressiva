2c4fc39bf97cc8e9b19b1be22b2cfa61d7cf0f9a
Airezigma <gabs.sfv@gmail.com>
2021-03-05 19:10:16 -0300

    regressiva

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {Console.WriteLine("Preparar...");
+
+
+Console.WriteLine("Pressione ENTER para continuar");
+
+
+Console.ReadKey();
+
+
+Console.Write("\n");
+
+
+
+
+
+Console.WriteLine("Apontar...");
+
+
+Console.WriteLine("Pressione ENTER para continuar");
+
+
+Console.ReadKey();
+
+
+Console.Write("\n");
+
+
+
+
+
+Console.WriteLine("FOGO!");
+
+
+Console.WriteLine("Pressione ENTER para sair");
+
+
+Console.ReadKey();
+    
+        }
+    }
+}
